bb9343ee32fa359c7c044733c018d36f560c02f0
Zach Green <zgreen@mit.edu>
2014-05-22 11:18:56 -0400

    index.php loop, with data attributes

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
+<?php 
+	get_header();
 
+	$i= 0;
+?>
 
+<?php  while ( have_posts() ): the_post(); ?>
 
+	<h2 class="title-post" data-post-number="<?php echo $i; $i++; ?>"><?php the_title(); ?></h2>
 
+<?php endwhile; ?>
 
+<?php 
+	get_footer();
+?>
