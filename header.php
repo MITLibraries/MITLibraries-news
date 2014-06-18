3e132eec7b3eead285a8df524058f0e0218ec5dc
Zach Green <zgreen@mit.edu>
2014-06-18 13:04:35 -0400

    add header and footer

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js">
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	<link rel="profile" href="http://gmpg.org/xfn/11" />
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,700,700italic' rel='stylesheet' type='text/css'>
+	<?php wp_head(); ?>
+</head>
+
+<body>
+
+	<h1 class="name-site"><?php echo get_bloginfo('name'); ?></h1>
