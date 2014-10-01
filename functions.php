8400451216ce5b7f9bba48e874e99c2e41a9778d
Zach Green <zgreen@mit.edu>
2014-10-01 16:56:04 -0400

    temporarily comment out ACF WP API encode
    Will reenable once ACF is activated site-by-site, rather than at the
    network level.

--- functions.php
+++ functions.php
@@ -138,3 +138,13 @@ function register_news_taxonomies() {
 	);
 }
 add_action( 'init', 'register_news_taxonomies' );
+
+
+// function wp_api_encode_acf($data,$post,$context){
+// 	$data['meta'] = array_merge($data['meta'],get_fields($post['ID']));
+// 	return $data;
+// }
+ 
+// if( function_exists('get_fields') ){
+// 	add_filter('json_prepare_post', 'wp_api_encode_acf', 10, 3);
+// }
