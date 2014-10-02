11bfbbe7582c17e52a40f8f5753e25e907d2af92
Zach Green <zgreen@mit.edu>
2014-10-02 16:17:22 -0400

    check if post is marked new on index.php

--- index.php
+++ index.php
@@ -7,7 +7,10 @@
 	<?php  while ( have_posts() ): the_post(); ?>
 	
 	<div class="post">
-		<h2 class="title-post"><?php the_title(); ?></h2>
+		<?php if (get_field('mark_as_new') === true): ?>
+		<span>New!</span>
+		<?php endif; ?>
+		<h2 class="title-post"><?php the_title(); ?>test</h2>
 		<?php the_content(); ?>
 	</div>
 	
