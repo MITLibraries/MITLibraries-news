8d4d952c2aaadb34198fae2acc017c4f59186b73
Zach Green <zgreen@mit.edu>
2014-10-02 15:37:59 -0400

    remove "post number" functionality

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
 <?php 
 	get_header();
-
-	$i= 0;
 ?>
 
 <div class="content-all">
 
 	<?php  while ( have_posts() ): the_post(); ?>
 	
-	<div class="post" data-post-number="<?php echo $i; $i++; ?>">
+	<div class="post">
 		<h2 class="title-post"><?php the_title(); ?></h2>
 		<?php the_content(); ?>
 	</div>
