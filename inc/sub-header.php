5d5d40b6e3a9ca2813da5626b3c8bf7538982e07
Zach Green <zgreen@mit.edu>
2014-10-08 18:41:55 -0400

    init sub-header inc file

---
+++ inc/sub-header.php
@@ -0,0 +1,10 @@
+<div class="title-page flex-container">
+	<h1 class="name-site"><?php bloginfo('name'); ?></h1>
+	<div class="flex-container">
+		<a href="">Subscribe</a>
+		<a href="">Twitter</a>
+		<a href="">Facebook</a>
+		<a href="">Google+</a>
+		<div class="search"><?php get_search_form('true'); ?></div>
+	</div>
+</div>
