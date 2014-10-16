073727f1655d5225ed091c509a25b4f18f143329
Zach Green <zgreen@mit.edu>
2014-10-16 13:18:32 -0400

    add basic index.php posts markup

--- index.php
+++ index.php
@@ -5,18 +5,37 @@
 <div class="content-main">
 
 	<?php get_template_part('inc/sub-header'); ?>
+	
+	<section class="posts--preview flex-container">
 
 	<?php  while ( have_posts() ): the_post(); ?>
-	
-	<div class="post">
+
+	<a href="<?php echo get_post_permalink(); ?>" class="post flex-container <?php if (!has_post_thumbnail()) { echo "no-image"; } ?>">
 		<?php if (get_field('mark_as_new') === true): ?>
 		<span>New!</span>
 		<?php endif; ?>
+		<?php 
+		if ( has_post_thumbnail() ) {
+			the_post_thumbnail();
+		}
+		?>
+
 		<h2 class="title-post"><?php the_title(); ?></h2>
-		<?php the_content(); ?>
-	</div>
+		<div class="excerpt-post">
+			<?php the_excerpt(); ?>
+		</div>
+		<div class="category-post">
+			<?php
+			$category = get_the_category(); 
+			echo $category[0]->cat_name;
+			echo $category[1]->cat_name;
+			?>
+		</div>
+	</a>
 	
-<?php endwhile; ?>
+	<?php endwhile; ?>
+
+	</section>
 
 </div>
 
