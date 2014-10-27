4571a69dd586cfd715e80c7485872dbbd039bf6e
Zach Green <zgreen@mit.edu>
2014-10-27 16:38:27 -0400

    add basic markup for post type links and dropdowns

--- inc/sub-header.php
+++ inc/sub-header.php
@@ -8,3 +8,27 @@
 		<div class="search"><?php get_search_form('true'); ?></div>
 	</div>
 </div>
+<div class="flex-container">
+	<nav class="types-posts">
+		<a class="button-secondary" href="#0">Show All</a>
+		<a class="button-secondary" href="#0">Events</a>
+		<a class="button-secondary" href="#0">News</a>
+		<a class="button-secondary" href="#0">Bibliotech</a>
+	</nav>
+	<form action="#0">
+		<select name="" id="">
+			<option value="">Category option</option>
+			<option value="">Category option</option>
+			<option value="">Category option</option>
+			<option value="">Category option</option>
+			<option value="">Category option</option>
+		</select>
+		<select name="" id="">
+			<option value="">Date option</option>
+			<option value="">Date option</option>
+			<option value="">Date option</option>
+			<option value="">Date option</option>
+			<option value="">Date option</option>
+		</select>
+	</form>
+</div>
