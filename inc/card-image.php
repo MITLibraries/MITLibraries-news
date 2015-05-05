61779d0f7eeed9a3f6b91497e2dbe29df9aa9b13
Matt Bernhardt <mjbernha@mit.edu>
2015-05-05 14:34:03 -0400

    Edits code comment
    Signed-off-by: Matt Bernhardt <mjbernha@mit.edu>

--- inc/card-image.php
+++ inc/card-image.php
@@ -1,5 +1,5 @@
 <?php
-  // This partial is called by render() as it builds the cards displayed throughout the news site
+  // This partial is called by render() as it builds the cards on the news site
   $image = "";
   if (get_field("listImg") != "") {
     $image = '<img data-original="' . get_field("listImg") . '" width="100%" height="111" class="img-responsive" alt="' . get_the_title() . '"/>';
