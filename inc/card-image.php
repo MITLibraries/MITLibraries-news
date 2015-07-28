dfbd0637c58960820741cbc2fa5b55536e7402c7
Matt Bernhardt <mjbernha@mit.edu>
2015-07-28 14:50:05 -0400

    Backs out of attempted abstraction of news card rendering
    Signed-off-by: Matt Bernhardt <mjbernha@mit.edu>

--- inc/card-image.php
+++
@@ -1,8 +0,0 @@
-<?php
-  // This partial is called by render() as it builds the cards on the news site
-  $image = "";
-  if (get_field("listImg") != "") {
-    $image = '<img data-original="' . get_field("listImg") . '" width="100%" height="111" class="img-responsive" alt="' . get_the_title() . '"/>';
-  }
-  echo $image;
-?>
