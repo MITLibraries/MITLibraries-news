b0ddf15ff2390c9afd4431ad5f6fa8d76f1701e2
pbruk <phillip.bruk@gmail.com>
2015-08-02 09:08:25 -0400

    Add H1

--- inc/bib-header.php
+++ inc/bib-header.php
@@ -1,9 +1,9 @@
 <div class="container biblio">
     	<div class="row">
       <div class="biblioBox col-sm-6 col-md-4">
-      <a href="/news/bibliotech-index/"> 
+      <h1><a href="/news/bibliotech-index/"> 
       <img class="centered" src="<?php echo get_theme_root_uri(); ?>/mit-libraries-news/images/biblioHeader.png" alt="Bibliotech Header" class="img-responsive" width="331" height="75">
-      </a> </div>
+      </a></h1> </div>
       <div class="biblioBox col-sm-6 col-md-4">
         <p>A biannual newsletter published by the MIT Libraries</p>
       </div>
