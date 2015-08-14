3dec436a7969ff3dd5a1d8b087640d12a19e7b79
pbruk <phillip.bruk@gmail.com>
2015-08-14 09:44:54 -0400

    Remove author description, author link

--- author.php
+++ author.php
@@ -33,7 +33,7 @@ get_header(); ?>
 			?>
 
 			<header class="author-archive-header">
-				<h1 class="lib-header"><?php printf( __( 'Author Archives: %s', 'twentytwelve' ), '<span class="vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( "ID" ) ) ) . '" title="' . esc_attr( get_the_author() ) . '" rel="me">' . get_the_author() . '</a></span>' ); ?></h1>
+				<h1 class="lib-header"><?php printf('Author: ' . '<strong>' . get_the_author('', false) . '</strong>' ); ?></h1>
 			</header><!-- .archive-header -->
 
 			<?php
@@ -48,7 +48,7 @@ get_header(); ?>
 
 			<?php
 			// If a user has filled out their description, show a bio on their entries.
-			if ( get_the_author_meta( 'description' ) ) : ?>
+/*			if ( get_the_author_meta( 'description' ) ) : ?>
 			<div class="author-info">
 				<div class="author-avatar">
 					<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'twentytwelve_author_bio_avatar_size', 60 ) ); ?>
