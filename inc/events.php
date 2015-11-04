313b295ecf410e495a31799aa8164223bd1a0a88
pbruk <phillip.bruk@gmail.com>
2015-11-04 11:25:07 -0500

    Add class for display purposes and bg image

--- inc/events.php
+++ inc/events.php
@@ -1,13 +1,19 @@
- <!--/EVENT  DATE-->
+<?php
+/**
+ * Template part for displaying EVENTS on cards.
+ *
+ *
+ */
+?>
+
           <?php if(get_field('event_date')){ 
 				$date = DateTime::createFromFormat('Ymd', get_field('event_date'));
 				
 			?>
-          
-          
-          <div class="events">
-		  <div class="event"> </div>
-		  <?php echo $date->format('F j, Y'); ?>
+          <!--EVENT --> 
+          <div class="events classCheck">
+	      <span class="bg-image"><img src="/wp-content/themes/libraries/images/calendar.svg" width="15px" height="15px"></span>    
+		  <span class="event"><?php echo $date->format('F j, Y'); ?></span> 
           <span class="time">
             <?php if( get_field('event_start_time') ){ 
 			  		echo the_field('event_start_time'); 
