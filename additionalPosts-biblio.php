3fe65a3b51b523653ac99f5cdcb3b4b171564348
pbruk <phillip.bruk@gmail.com>
2015-11-07 23:43:53 -0500

    Restructure, standardize templates

--- additionalPosts-biblio.php
+++ additionalPosts-biblio.php
@@ -44,7 +44,6 @@ $(document).ready(function() {
 
 
 ?>
-  <div class="row">
   
   
  <?php
@@ -57,8 +56,7 @@ $("#another").hide();
 </script>
 
 
-<?php } 
-//removes button end ?>
+<?php } //removes button end ?>
  
   
   
@@ -71,45 +69,8 @@ $theLength = $my_query->post_count;
 $i++; 
 ?>
 
-
-  <div class="<?php if ($i % 3 == 0){ echo "third "; } ?> col-xs-12  col-xs-B-6 col-sm-4 col-md-4 no-padding-left-mobile">
-        <div class="flex-item blueTop eventsBox <?php if (get_field("listImg")) { echo "has-image";} else { echo "no-image"; } ?>" onClick='location.href="<?php if((get_field("external_link") != "") && $post->post_type == 'spotlights'){ the_field("external_link");}else{ echo get_post_permalink();}  ?>"'>
-        <?php
-		if (get_field("listImg") != "" ) { ?>
-        <img data-original="<?php the_field("listImg") ?>" width="100%" height="111" class="img-responsive"  alt="<?php the_title(); ?>"/>
-        <?php } ?>
-        
-        
-        <h2 class="entry-title title-post  <?php if($post->post_type == 'spotlights'){ echo "spotlights"; } ?>">
-          <?php  the_title(); ?>
-        </h2>
-        
-        
-    	 <?php get_template_part('inc/events'); ?>
-        
-        <?php get_template_part('inc/entry'); ?>
-
-        <!--final **** else-->
-        <?php {  ?>
-        <!--EVENT -->
-        <?php } ?>
-        
-        
-        
-        <div class="category-post">
-          <?php	
-        		$category = get_the_category();     
-				$rCat = count($category);
-				$r = rand(0, $rCat -1);
-	
-				echo '<a title="'.$category[$r]->cat_name.'"  title="'.$category[$r]->cat_name.'" href="'.get_category_link($category[$r]->term_id ).'">'.$category[$r]->cat_name.'</a>';
-		 ?>
-          <span class="mitDate">
-          <time class="updated"  datetime="<?php echo get_the_date(); ?>">&nbsp;&nbsp;<?php echo get_the_date(); ?></time>
-          </span> </div>
-      </div>
-    </div>
+    <?php renderBiblioCard( $i, $post ); ?>
+  
 <?php endwhile; ?>
 <?php endif; ?>
 <?php wp_reset_query();  // Restore global post data stomped by the_post(). ?>
-</div>
