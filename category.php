77d6ad4df020ce1ed2c64a45d965d218d96fd5c2
pbruk <phillip.bruk@gmail.com>
2015-11-13 12:34:08 -0500

    Restructure, standardize template to include render functions

--- category.php
+++ category.php
@@ -21,7 +21,7 @@ $date = DateTime::createFromFormat('Ymd', get_field('event_date'));
 
   <div id="content" role="main">
     <?php if ( have_posts() ) : ?>
-    <div class="container">
+    <div class="container container-fluid">
       <div class="row">
 	      <?php 
 	if(is_category()){
@@ -32,69 +32,23 @@ $date = DateTime::createFromFormat('Ymd', get_field('event_date'));
 			$i = -1;
 			while ( have_posts() ) : the_post();
 			$i++; 
+                renderRegularCard( $i, $post ); // --- CALLS REGULAR CARDS --- //
 			?>
-        <div class="<?php if ($i % 3 == 0){ echo "third "; } ?> col-xs-12  col-xs-B-6 col-sm-4 col-md-4 no-padding-left-mobile">
-          <div class="hentry flex-item blueTop eventsBox <?php  if (get_field("listImg")) { echo "has-image";} else { echo "no-image"; } ?>" onClick='location.href="<?php if((get_field("external_link") != "") && $post->post_type == 'spotlights'){ the_field("external_link");}else{ echo get_post_permalink();}  ?>"'>
-            <?php get_template_part('inc/spotlights'); ?>
-            <?php
-		if (get_field("listImg") != "" ) { ?>
-            <img data-original="<?php the_field("listImg") ?>" width="100%" height="111" class="img-responsive"  alt="<?php the_title(); ?>"/>
-            <?php } ?>
-            <?php if($post->post_type == 'spotlights'){ ?>
-			 <h2 class="entry-title title-post spotlights">
-          <a href="<?php the_field("external_link"); ?>"><?php the_title();?></a>
-        </h2> 
-		<?php }else{ ?>
-        <h2 class="entry-title title-post">
-          <a href="<?php the_permalink(); ?>"><?php the_title();?></a>
-        </h2>
-        <?php 	} ?>
-            <?php get_template_part('inc/events'); ?>
-            <?php get_template_part('inc/entry'); ?>
+ 
+			<?php if ( get_post_type( get_the_ID() ) == 'bibliotech' ) { ?>
+           
+            <?php } //get_post_type( get_the_ID() ) == 'bibliotech' ?>
+              
+                    <?php  wp_reset_query(); // Restore global post data stomped by the_post(). ?>
+           
+                    <?php endwhile; ?>
             
-            <!--final **** else-->
-            <?php {  ?>
-            <!--EVENT -->
-            <?php } ?>
-            
-            
-              <div class="category-post <?php  if(get_post_type( get_the_ID() ) == 'bibliotech'){ echo "Bibliotech";} ?>">
-<?php 
-  if(get_post_type( get_the_ID() ) == 'bibliotech'){
-	   echo "<div class='bilbioImg bilbioTechIcon'>
-	   </div>";
-	   echo "<div class='biblioPadding'>&nbsp;<a href='/news/bibliotech-index/' title='Bibliotech'>Bibliotech</a>"; ?>
-	   
-	    <span class="mitDate">
-          <time class="updated"  datetime="<?php echo get_the_date(); ?>">&nbsp;&nbsp;<?php echo get_the_date(); ?></time>
-          </span> </div> 
-	   
-	<?php 	  }else{
-				$category = get_the_category();
-				$categoryID = $category->cat_ID;
-				$rCat = count($category);
-				$r = rand(0, $rCat -1);
-				echo '<a title="'.$category[$r]->cat_name.'"  title="'.$category[$r]->cat_name.'" href="'.get_category_link($category[$r]->term_id ).'">'.$category[$r]->cat_name.'</a>'; ?>
-	 
-          <span class="mitDate">
-          <time class="updated"  datetime="<?php echo get_the_date(); ?>">&nbsp;&nbsp;<?php echo get_the_date(); ?></time>
-          </span> </div> 
-        <?php  } ?>
-       </div><!--last-->
-    </div>
-    <?php  if(get_post_type( get_the_ID() ) == 'bibliotech'){ ?>
-    </div><!--this div closes the open div in biblio padding-->
-    <?php } ?>      
- <?php 
-		 wp_reset_query();  
-		endwhile; ?>
-        <?php else : ?>
-        <?php //get_template_part( 'content', 'none' ); ?>
-        <?php endif; ?>
-      </div>
-      <!--closeMITcontainer--> 
-    </div>
-    <!--closes row-->
+                    <?php endif; ?>
+  
+              </div>
+              <!--closes ROW-->
+              </div>
+              <!--closes NEWS-CONTAINER-->
     
      <?php get_template_part('inc/more-posts'); ?>
   </div>
