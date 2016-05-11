acf2283ff91fbb1c65e9c1b5396c3f127b9ab985
pbruk <phillip.bruk@gmail.com>
2016-05-11 10:36:29 -0400

    Remove white space.

--- inc/sub-headerException.php
+++ inc/sub-headerException.php
@@ -8,7 +8,7 @@
  * @since Twenty Twelve 1.0
  */
 	?>
-	 
+	
 	<div class="newsSubHeader clearfix">
 		
 		<div class="innerPadding clearfix">
@@ -18,25 +18,27 @@
 				<?php if ( is_single($post)) { ?>
 				
 					<h2 class="name-site"><a href="/news/">News &amp; events</a></h2>
-	      
+	
 				<?php } else { ?>
-	    
+	
 					<h1 class="name-site"><a href="/news/">News &amp; events</a>
 		
 				<?php  if (is_category()) { 
+					
 					printf('<span>'. ': ' . single_cat_title( '', false ) . '</span>' );
-				} ?>        
+				
+				} ?>
 				
 					</h1>
 		
 				<?php } ?>
-	    
-	 		</div><!--title-page-->   
 	
-	 		<div class="socialNav singleSocialNav hidden-xs not_on_phone socialNav 
-		 		col-xs-12 col-sm-8 col-md-7 col-lg-7 clearfix ">
-	 			
-	 			<?php get_template_part('inc/social'); ?>
+			</div><!--title-page-->
+	
+			<div class="socialNav singleSocialNav hidden-xs not_on_phone socialNav 
+				col-xs-12 col-sm-8 col-md-7 col-lg-7 clearfix ">
+				
+				<?php get_template_part('inc/social'); ?>
 	
 			</div><!--.socialnav-->
 	
