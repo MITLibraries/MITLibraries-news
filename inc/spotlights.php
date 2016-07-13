742d4e4b2323ffa293b14a26393bfcd20676f7e7
pbruk <phillip.bruk@gmail.com>
2016-07-12 21:14:43 -0400

    Changes for code-climate purposes.

--- inc/spotlights.php
+++ inc/spotlights.php
@@ -3,40 +3,40 @@
 			$value = get_field('feature_type');
 			$label = $field['choices'][ $value ];
 			//print_r($label);
-			if(($field['choices'][ $value ]) == "Tip"){
+			if( ($field['choices'][ $value ] ) === 'Tip' ) {
 				$featImg = '<div class="info"></div>';
 				}
-			if(($field['choices'][ $value ]) == "Update"){
+			if( ($field['choices'][ $value ] ) === 'Update' ) {
 			$featImg ='<div class="update"></div>';
 				}
-			if(($field['choices'][ $value ]) == "Featured resource"){
+			if( ($field['choices'][ $value ] ) === 'Featured resource' ) {
 					$featImg ='<div class="or_star-25"></div>';
 				}
-			if(($field['choices'][ $value ]) == "Featured collection"){
+			if( ($field['choices'][ $value ] ) === 'Featured collection' ) {
 				$featImg ='<div class="or_star-25"></div>';
 				}
-			if(($field['choices'][ $value ]) == "Featured service"){
+			if( ($field['choices'][ $value ] ) === 'Featured service' ) {
 					$featImg ='<div class="or_star-25"></div>';
 				}
-			if(($field['choices'][ $value ]) == "Featured exhibit"){
+			if( ($field['choices'][ $value ] ) === 'Featured exhibit' ) {
 				$featImg ='<div class="or_star-25"></div>';
 				}
-			if(($field['choices'][ $value ]) == "Featured story"){
+			if( ($field['choices'][ $value ] ) === 'Featured story' ) {
 					$featImg ='<div class="or_star-25"></div>';
 				}
-			if(($field['choices'][ $value ]) == "Featured video"){
+			if( ($field['choices'][ $value ] ) === 'Featured video' ) {
 				$featImg ='<div class="or_star-25"></div>';
 				}
-			if(($field['choices'][ $value ]) == "In the media"){
+			if( ($field['choices'][ $value ] ) === 'In the media' ) {
 					$featImg ='<div class="or_star-25"></div>';
 				}
-			if(($field['choices'][ $value ]) == "Check it out"){
+			if( ($field['choices'][ $value ] ) === 'Check it out' ) {
 				$featImg ='<div class="or_star-25"></div>';
 				}
 		  ?>
-<?php if($post->post_type == 'spotlights'){ ?>
+<?php if($post->post_type == 'spotlights') { ?>
 <div class="featuredCol"><?php echo $label; ?></div>
 <?php } ?>
-<?php if($post->post_type == 'spotlights'){ ?>
+<?php if($post->post_type == 'spotlights') { ?>
 <div class="featuredColImg"> <?php echo $featImg; ?> </div>
 <?php } ?><!--//spotlight --> 
