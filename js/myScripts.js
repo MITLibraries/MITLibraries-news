caf07bf9ee76ed470eba1a5d23114e0171faac00
Matt Bernhardt <mjbernha@mit.edu>
2016-08-30 15:46:30 -0400

    Clean myScripts.js, update for new syntaxes
    Signed-off-by: Matt Bernhardt <mjbernha@mit.edu>

--- js/myScripts.js
+++ js/myScripts.js
@@ -1,40 +1,27 @@
-(function($) {
-    
-	//lazy loading
-	$("img.img-responsive").lazyload({ 
-    effect : "fadeIn", 
-    effectspeed: 450 ,
-	failure_limit: 999999
-  }); 
+var window, jQuery, APloader;
 
- 	//category force selection of all news
+(function($) {
+
+	//lazy loading
+	$("img.img-responsive").lazyload({
+		effect : "fadeIn",
+		effectspeed: 450 ,
+		failure_limit: 999999
+	});
+
+	//category force selection of all news
 	$('input:checkbox[id=in-category-43]').attr('checked',true);
 
 	// Additional Posts loader
-	var APloader = window.mitlibnews.loader;
+	APloader = window.mitlibnews.loader;
 	// TODO: is initialization necessary?
 	APloader.initialize();
 	// TODO: add parameters for what type of load, and offset/posts_per_page
-	APloader.loadPosts(10, 9);
+	APloader.loadPosts(9);
+
+	$("#mitlibnews-another").click(function() {
+		// TODO: read data attribute in 'show more' markup that determines what type of 'more' to show
+		APloader.loadPosts(9);
+	});
 
 })(jQuery);
-
-
-
-
-
-
-  
-  
-
-
-	
-
-  
-  
-  
-  
-
-	
-
-
