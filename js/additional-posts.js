9cedcd5785cc7605bc8932807ea9a88be2af15f1
Matt Bernhardt <mjbernha@mit.edu>
2016-08-30 15:45:46 -0400

    Add render function, postcontent getter/setter, and other details
    Signed-off-by: Matt Bernhardt <mjbernha@mit.edu>

--- js/additional-posts.js
+++ js/additional-posts.js
@@ -7,48 +7,66 @@
  *
  */
 
+var window, document, jQuery;
+
 window.mitlibnews = window.mitlibnews || {};
 
 window.mitlibnews.loader = {
 
 	/**
+	 * Properties
+	 */
+	container : '',
+	offset: 0,
+	page: 1,
+	postcontent: '',
+
+	/**
 	 * Initialize
 	 */
 	initialize : function() {
-		console.log('MIT Libraries News Loader initialized');
+		console.log('MIT Libraries News Loader initializing...');
+		// Identify container that will receive post cards.
+		this.container = document.getElementById('mitlibnews-container');
+		// The type of query is identified by data attribute on the container.
+		this.setPostcontent();
+		console.log('Initialization complete.');
 	},
 
 	/**
 	 * Simple Post Loader
 	 */
-	loadPosts : function(offset, posts_per_page) {
-		$.ajax({
+	loadPosts : function(posts_per_page) {
+		console.log('Loading ' + posts_per_page + ' posts');
+		console.log('Filter: ');
+		var filter = {
+			'posts_per_page': posts_per_page,
+		};
+		console.log(filter);
+		jQuery.ajax({
 			url: '/news/wp-json/posts',
 			data: {
-				filter: {
-					'posts_per_page': posts_per_page,
-					'offset': offset,
-				}
+				filter: filter,
+				page: this.page,
 			},
 			dataType: 'json',
 			type: 'GET',
 			success: function(data) {
-				$.each(data, function( index, value ) {
-					card = document.createElement( "div" );
-					$(card).addClass( "no-padding-left-mobile col-xs-12 col-xs-B-6 col-sm-6 col-md-4 col-lg-4" );
-					cardBody = document.createElement( "div" );
-					$(cardBody).addClass( "flex-item blueTop eventsBox no-image" );
-					$(card).append( cardBody );
-					$(cardBody).append( document.createTextNode( value.ID ) );
-					$(cardBody).append( document.createTextNode( "<br>" ) );
-					$(cardBody).append( document.createTextNode( value.title ) );
-					$("#mitlibnews").append( card );
+				console.log(posts_per_page + ' posts loaded');
+				// identify targeted container
+				var target = window.mitlibnews.loader.getContainer();
+				jQuery.each(data, function( index, value ) {
+					jQuery(target).append( window.mitlibnews.loader.renderCard(value) );
 				});
+				window.mitlibnews.loader.setPage( window.mitlibnews.loader.getPage() + 1);
+				console.log('New page: ');
+				console.log(window.mitlibnews.loader.getPage());
 			},
 			error: function() {
 				console.log("Error");
 			}
 		});
+		console.log('');
 	},
 
 	/**
@@ -56,7 +74,7 @@ window.mitlibnews.loader = {
 	 */
 	loadArchive : function() {
 		console.log('Loading Archive posts...');
-		$.ajax({
+		jQuery.ajax({
 			url: '/news/wp-json/posts',
 			data: {
 				filter: {
@@ -99,4 +117,104 @@ window.mitlibnews.loader = {
 	 * Load More - Search Results
 	 */
 
-}
+	/**
+	 * Card Renderer
+	 *
+	 * This takes a JSON object representing a post, and returns relevant markup
+	 */
+	renderCard : function(post) {
+		var card, cardBody, cardContainer, cardCategory;
+		console.log(post);
+
+		// Card outer element
+		card = document.createElement( 'div' );
+		// TODO: Is this string of classes standard?
+		jQuery(card).addClass( 'no-padding-left-mobile col-xs-12 col-xs-B-6 col-sm-6 col-md-4 col-lg-4' );
+
+		// Card inner element
+		cardBody = document.createElement( 'div' );
+		// TODO: Is this string of classes standard?
+		jQuery(cardBody).addClass( 'flex-item blueTop eventsBox no-image' );
+		// TODO: need an onClick attribute?
+		jQuery(card).append( cardBody );
+
+		// interiorCardContainer
+		cardContainer = document.createElement( 'div' );
+		jQuery(cardContainer).addClass( 'interiorCardContainer' );
+		jQuery(cardBody).append( cardContainer );
+
+		jQuery(cardContainer).append( document.createTextNode( post.ID ) );
+		jQuery(cardContainer).append( document.createElement( 'br' ) );
+		jQuery(cardContainer).append( document.createTextNode( post.title ) );
+
+		// category element
+		cardCategory = document.createElement( 'div' );
+		jQuery(cardCategory).addClass( 'category-post' );
+		// TODO: Bibliotech class
+		jQuery(cardBody).append( cardCategory );
+
+		return card;
+	},
+
+	/**
+	 * Get Container
+	 */
+	getContainer : function() {
+		return document.getElementById('mitlibnews-container');
+	},
+
+	/**
+	 * Get Offset
+	 */
+	getOffset : function() {
+		console.log("Retrieving offset value of " + this.offset);
+		return this.offset;
+	},
+
+	/**
+	 * Get Page
+	 */
+	getPage : function() {
+		return this.page;
+	},
+
+	/**
+	 * Get Container
+	 */
+	getPostcontent : function() {
+		return this.postcontent;
+	},
+
+	/**
+	 * Set Offset
+	 */
+	setOffset : function(value) {
+		console.log("Replacing offset value of " + this.offset + " with " + value);
+		this.offset = value;
+		return this.getOffset();
+	},
+
+	/**
+	 * Set Page
+	 */
+	setPage : function(value) {
+		console.log("Replacing page value of " + this.page + " with " + value);
+		this.page = value;
+	},
+
+	/**
+	 * Set Post Content
+	 */
+	setPostcontent : function(value) {
+		// The default value is 'all'.
+		this.postcontent = 'all';
+		// If everything is not set, just use the default.
+		if ( !this.container || !this.container.dataset || !this.container.dataset.postcontent ) {
+			console.log( 'Post content attribute not found! Using default value.' );
+			return true;
+		}
+		// If we're still here, use the real value.
+		this.postcontent = this.container.dataset.postcontent;
+		return true;
+	}
+};
