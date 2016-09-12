eb0f4706a667eacb14adcb2d99b7a0d282d30ad5
Matt Bernhardt <mjbernha@mit.edu>
2016-09-12 15:27:54 -0400

    Adds initialization step

--- js/src/mitlibnews.loader.js
+++ js/src/mitlibnews.loader.js
@@ -1,11 +1,13 @@
+var document;
+
 function Loader(params) {
 	"use strict";
-
-	console.log('Loader here');
 
 	// Default parameters
 	// Container is the HTML element to which everything is added.
 	this.container = 'mitlibnews-container';
+	// Postcontainer is DOM element corresponding to Container.
+	this.postcontainer = '';
 	// Page is the counter used for pagination.
 	this.page = 1;
 	// Pagesize sets how many articles are loaded in a page
@@ -30,10 +32,55 @@ function Loader(params) {
 	}
 
 	// Connect to markup, read data attributes
+	this.initialize();
+
 }
 
 Loader.prototype = {
 	constructor: Loader,
+
+	// Initialization
+	initialize : function() {
+		// Connect to markup
+		this.postcontainer = document.getElementById( this.getContainer() );
+
+		// If no post container is found, then we quit and do nothing further.
+		if( !this.postcontainer ) {
+			return false;
+		}
+
+		// Read data attributes
+		this.loadAttributes();
+
+		// Load posts
+		this.loadPosts();
+	},
+
+	// Load attributes
+	loadAttributes : function() {
+		// If the post container doesn't have data attributes, then leave the defaults
+		if ( !this.postcontainer || !this.postcontainer.dataset ) {
+			console.log('Container or data attributes not found - leaving defaults');
+			return true;
+		}
+
+		if ( this.postcontainer.dataset.pagesize ) {
+			console.log('Overriding page size');
+			this.setPagesize( this.postcontainer.dataset.pagesize );
+		}
+
+		if ( this.postcontainer.dataset.postcontent ) {
+			console.log('Overriding post content');
+			this.setPostcontent( this.postcontainer.dataset.postcontent );
+		}
+
+		return true;
+	},
+
+	// Load posts
+	loadPosts : function() {
+		console.log('Loading ' + this.getPagesize() + ' posts (page ' + this.getPage() + ')');
+	},
 
 	// Container getter and setter
 	getContainer : function() {
