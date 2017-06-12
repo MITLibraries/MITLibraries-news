e404c7ee95309a5b18c67ccd71e5f5b055cbcca7
Matt Bernhardt <mjbernha@mit.edu>
2017-06-12 16:30:57 -0400

    Adds initial call to fake-API, triggering lazyload plugin

--- js/myScripts.js
+++ js/myScripts.js
@@ -1,35 +1,13 @@
-
-
 (function($) {
-    
-	//lazy loading
-	$("img.img-responsive").lazyload({ 
-    effect : "fadeIn", 
-    effectspeed: 450 ,
-	failure_limit: 999999
-  }); 
 
- 	//category force selection of all news
+	//lazy loading
+	$("img.img-responsive").lazyload({
+		effect: "fadeIn",
+		effectspeed: 450,
+		failure_limit: 999999
+	});
+
+	//category force selection of all news
 	$('input:checkbox[id=in-category-43]').attr('checked',true);
 
 })(jQuery);
-
-
-
-
-
-
-  
-  
-
-
-	
-
-  
-  
-  
-  
-
-	
-
-
