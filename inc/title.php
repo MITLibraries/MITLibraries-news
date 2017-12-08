7f78685643419b89d514c7c1308315570335b899
Matt Bernhardt <mjbernha@mit.edu>
2017-12-08 11:02:44 -0500

    Reformats inc/title.php a bit

--- inc/title.php
+++ inc/title.php
@@ -8,19 +8,23 @@
 
 ?>
 
-		<?php if ( 'spotlights' == $post->post_type ) { ?>
-			 <h2 class="entry-title title-post spotlights">
-			 <a href="<?php the_field( 'external_link' ); ?>"><?php the_title();?></a>
-			</h2> 
-		<?php } else { 
+<?php
+if ( 'spotlights' === $post->post_type ) {
+?>
+	<h2 class="entry-title title-post spotlights">
+		<a href="<?php the_field( 'external_link' ); ?>"><?php the_title();?></a>
+	</h2>
+<?php
+} else {
+	$event_link = get_post_permalink();
 
-		$eventLink = get_post_permalink();
-
-		if ( get_field( 'calendar_url' ) ) { 
-			$eventLink  = 	get_field( 'calendar_url' );
-		}
-		 ?>
-			<h2 class="entry-title title-post classCheck">
-			<a href="<?php echo $eventLink; ?>"><?php the_title();?></a>
-			</h2>
-		<?php } ?>
+	if ( get_field( 'calendar_url' ) ) {
+		$event_link = get_field( 'calendar_url' );
+	}
+?>
+	<h2 class="entry-title title-post classCheck">
+		<a href="<?php echo esc_url( $event_link ); ?>"><?php the_title(); ?></a>
+	</h2>
+<?php
+}
+?>
