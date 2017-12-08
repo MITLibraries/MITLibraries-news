f23e3b203cf36bf1b57fd4ca0db21b66b78c7806
Matt Bernhardt <mjbernha@mit.edu>
2017-12-08 10:54:04 -0500

    Cleans formatting of inc/imageEvent.php a bit

--- inc/imageEvent.php
+++ inc/imageEvent.php
@@ -3,17 +3,19 @@
  * Template-part for displaying IMAGES on CARDS fom Calendar.mit.edu.
  *
  * @package MITLibraries-News
- * @since 1.0
+ * @since 1.6.0
  */
 
 ?>
 
-	<?php
-
-		if ( get_field( 'calendar_image' ) != '' ) { ?>
-			<div class="card-image classCheck">
-				<div id='crop_the_image'>
-					<img src="<?php the_field( 'calendar_image' ) ?>"  alt="<?php the_title();?>"/>
-				</div>
-			</div>
-		<?php } ?>
+<?php
+if ( get_field( 'calendar_image' ) !== '' ) {
+?>
+	<div class="card-image classCheck">
+		<div id='crop_the_image'>
+			<img src="<?php the_field( 'calendar_image' ); ?>"  alt="<?php the_title(); ?>"/>
+		</div>
+	</div>
+<?php
+}
+?>
